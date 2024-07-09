8a0c2086fb0b2bdce18b190bcb2a3e1ed9936444
arshcherbakov <shcherbakov.fortech@gmail.com>
2024-07-09 10:51:29 +0300

    fix: alphabet store

--- word-with-the-most-points.js
+++ word-with-the-most-points.js
@@ -1,18 +1,33 @@
-const createAlphabet = () => {
-  const alphabet = [];
-  let intexArr = 0;
-
-  for (let index = "a".charCodeAt(); "z".charCodeAt() >= index; index++) {
-    alphabet[intexArr] = String.fromCharCode(index);
-    intexArr++;
-  }
-
-  return alphabet;
+const alphabet = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
 };
 
 const high = (text) => {
-  const alphabet = createAlphabet();
-
   const arrayWord = text.split(" ");
 
   let maxBal = 0;
@@ -22,7 +37,7 @@ const high = (text) => {
     let sumBal = 0;
 
     for (let char of word) {
-      sumBal += alphabet.indexOf(char) + 1;
+      sumBal += alphabet[char] + 1;
     }
 
     if (sumBal > maxBal) {
